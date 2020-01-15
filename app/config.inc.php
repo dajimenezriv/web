b08ddfadbd780b71c27a260670821fea6754a575
Daniel Jiménez Rivera <zepovopnewbie@gmail.com>
2020-01-15 19:11:40 +0100

    Update config.inc.php

--- app/config.inc.php
+++ app/config.inc.php
@@ -1,17 +1,8 @@
 <?php
 
-/*
 define('NOMBRE_SERVIDOR', 'localhost');
-define('NOMBRE_USUARIO', 'id5422536_zepovop');
-define('PASSWORD', 'dbDibujante1234');
-define('NOMBRE_BBDD', 'id5422536_geek');
-
-define('SERVIDOR', 'http://www.geekzepovop.000webhostapp.com/');
-*/
-
-define('NOMBRE_SERVIDOR', 'localhost');
-define('NOMBRE_USUARIO', 'geek');
-define('PASSWORD', 'geek');
+define('NOMBRE_USUARIO', 'root');
+define('PASSWORD', '');
 define('NOMBRE_BBDD', 'geek');
 
 define('SERVIDOR', 'http://localhost/geek/');
